6fcace489d42d33dcf84e9444a765eb59a228727
franzromanov <114895290+franzromanov@users.noreply.github.com>
2024-12-08 22:38:49 +0800

    Update main.ino

--- MQ2xTurbidity/main.ino
+++ MQ2xTurbidity/main.ino
@@ -2,18 +2,33 @@
 #include "menu.h"
 #include "sense.h"
 
+unsigned long startClock,currentClock;
+const unsigned NormalDuration=7200000;
+
 void setup() {
+
   NET_START("IN UTERO", "coppernotgood666");
- 
+  startClock=millis();
+  Serial.println("Clock Up"); //change to oled
 }
 
   
 void loop(){
+  currentClock=millis();
 
 
+  //NORMAL_CONDITION>>>
+  if (currentClock - startClock >= NormalDuration) {
+    
+    //subProcess:sendToSheet
+    Serial.println("SeNdInG To GsHeEt...");//change to oled
+    SEND_TO_GSHEET(smokeState(12),turbidityReading(13));
 
-     //subProcess:sendToSheet
-     SEND_TO_GSHEET(smokeState(12),turbidityReading(13));
-     //subProcess:sendToTelegram
-     SEND_TO_TELEGRAM(smokeState(12),turbidityReading(13))
+    //subProcess:sendToTelegram
+    Serial.println("SeNdInG To TeLeGrAm...");//change to oled
+    SEND_TO_TELEGRAM(smokeState(12),turbidityReading(13));
+    startClock = millis(); // Reset timer for another 2 hours (optional)
+  }
+
+     
 }
